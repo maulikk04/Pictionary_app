27f23313f5c4944df2f249f49f96f462ecaed472
maulikk04 <118893402+maulikk04@users.noreply.github.com>
2024-02-11 17:05:40 +0530

    modify the frontend

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { log } = require('node:console');
+const {createServer}  = require('node:http');
+const {Server} = require('socket.io');
 const app = express();
 
 app.use(express.static('public'));
@@ -6,13 +9,25 @@ app.set('view engine', 'ejs');
 
 async function start() {
     try {
+        const server = createServer(app);
+        const io = new Server(server,{
+            connectionStateRecovery:{}
+        })
+        io.on('connection', async(socket)=>{
+            console.log('user connected');
+            socket.on('username' , (name)=>{
+                socket.username= name;
+                console.log(`${name} connected`);
+                io.emit('username',name);
+            })
+            app.get('/draw',(req,res)=>{
+                res.render('draw' , {username:socket.username});
+            })
+        })
         app.get('/', (req, res) => {
             res.render('home');
         })
-        app.get('/draw',(req,res)=>{
-            res.render('index')
-        })
-        app.listen('5000', () => {
+        server.listen('5000', () => {
             console.log("Server is running on port 5000");
         })
 
